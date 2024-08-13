135924616964dec5c648eebb9786661deb73d10f
Anagha RJ <anagharj7@gmail.com>
2024-08-13 10:10:27 +0530

    .

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function setActive(element) {
+    // Remove 'active' class from all links
+    const links = document.querySelectorAll('.navbar a');
+    links.forEach(link => link.classList.remove('active'));
+
+    // Add 'active' class to the clicked link
+    element.classList.add('active');
+}
